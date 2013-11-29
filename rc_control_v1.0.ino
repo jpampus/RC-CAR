08c9d3bc8405960bc4c5838b84e245c06762618e
s3rr00 <julian.pampus@gmail.com>
2013-11-29 02:55:59 -0800

    Update rc_control_v1.0.ino
    Fix layout

--- rc_control_v1.0.ino
+++ rc_control_v1.0.ino
@@ -4,12 +4,14 @@
 //   
 //	   
 //
-//	[1]	[3]
-//	  [ ] 
+//  [1] [3]
+//    [ ] 
 //    [ ]
 //  [2] [4]
 //
 //
+
+
 #define MOTOR1 3
 #define MOTOR1_DIRECTION 2
 
@@ -26,7 +28,8 @@
 //#define TIME_BACKWARDS 3000
 //#define TIME_TURN 1700 //Originally 1200
 
-void setup(){
+void setup()
+{
 	//MOTOR1 Controls
 	pinMode (MOTOR1, OUTPUT);
 	pinMode (MOTOR1_DIRECTION, OUTPUT);
@@ -41,38 +44,48 @@ void setup(){
 	pinMode (MOTOR4_DIRECTION, OUTPUT);
 }
 
-void loop() {
-
+void loop()
+{
   int left_pot = analogRead(A5);
   int right_pot = analogRead(A4);
 
-	if(left_pot<256) {
-	analogWrite (MOTOR1, 200);
-	analogWrite (MOTOR2, 200);
-	digitalWrite (MOTOR1_DIRECTION, 1); 
-	digitalWrite (MOTOR2_DIRECTION, 0);
-	} else if(left_pot>768) {
-			analogWrite (MOTOR1, 200);
-			analogWrite (MOTOR2, 200);
-			digitalWrite (MOTOR1_DIRECTION, 0); 
-			digitalWrite (MOTOR2_DIRECTION, 1);
-			} else {
-				analogWrite (MOTOR1, 0);
-				analogWrite (MOTOR2, 0);
-			}
+	if(left_pot<256)
+	{
+	  analogWrite (MOTOR1, 200);
+	  analogWrite (MOTOR2, 200);
+	  digitalWrite (MOTOR1_DIRECTION, 1); 
+	  digitalWrite (MOTOR2_DIRECTION, 0);
+	}
+	  else if(left_pot>768)
+	  {
+	    analogWrite (MOTOR1, 200);
+	  	analogWrite (MOTOR2, 200);
+	  	digitalWrite (MOTOR1_DIRECTION, 0); 
+	  	digitalWrite (MOTOR2_DIRECTION, 1);
+  	}
+    	else
+  	{
+	    analogWrite (MOTOR1, 0);
+	  	analogWrite (MOTOR2, 0);	
+	  }
 
-	if(right_pot<256) {
-	analogWrite (MOTOR3, 200);
-	analogWrite (MOTOR4, 200);
-	digitalWrite (MOTOR3_DIRECTION, 1); 
-	digitalWrite (MOTOR4_DIRECTION, 0);
-	} else if(right_pot>768) {
-			analogWrite (MOTOR3, 200);
-			analogWrite (MOTOR4, 200);
-			digitalWrite (MOTOR3_DIRECTION, 0); 
-			digitalWrite (MOTOR4_DIRECTION, 1);
-			} else {
-				analogWrite (MOTOR3, 0);
-				analogWrite (MOTOR4, 0);
-			}
+	if(right_pot<256)
+	{
+	  analogWrite (MOTOR3, 200);
+	  analogWrite (MOTOR4, 200);
+	  digitalWrite (MOTOR3_DIRECTION, 1); 
+	  digitalWrite (MOTOR4_DIRECTION, 0);
+	}
+	  else if(right_pot>768)
+	  {
+	    analogWrite (MOTOR3, 200);
+	  	analogWrite (MOTOR4, 200);
+	  	digitalWrite (MOTOR3_DIRECTION, 0); 
+	  	digitalWrite (MOTOR4_DIRECTION, 1);
+  	}
+	    else
+	    {
+	      analogWrite (MOTOR3, 0);
+	    	analogWrite (MOTOR4, 0);
+    	}
 }
